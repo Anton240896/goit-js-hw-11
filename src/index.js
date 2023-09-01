7f94ffd4a941884a0be15cf9b72bc7369d21a2a3
Anton240896 <antalipov24081111111@gmail.com>
2023-09-01 09:19:32 +0300

    add style,js...

--- src/index.js
+++ src/index.js
@@ -1,28 +1,215 @@
-  import axios from "axios";
-  import { Notify } from "notiflix";
-  import { searchPicture } from "./search-api";
-  import SimpleLightbox from 'simplelightbox';
-  import "simplelightbox/dist/simple-lightbox.min.css"
+ /*===================IMPORT=========================*/
+import { Notify } from 'notiflix';
+const axios = require('axios').default;
+import SimpleLightbox from 'simplelightbox';
+ import 'simplelightbox/dist/simple-lightbox.min.css';
 
-  const API_KEY = '39074822-7a439c7ecb254f2e87bade55b';
-  axios.defaults.headers.common['x-api-key'] = API_KEY
+ /*===================LET=========================*/
 
-  const lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: '250',
-    });
- /*============================================*/
+let searchQueryResult = '';
+let q = '';
+let pageOne = 1;
 
 
+
+ /*==================PIXABAYAPI==========================*/
+
+const pixabayAPI = {
+
+        baseUrl: 'https://pixabay.com/api/',
+        key: '39074822-7a439c7ecb254f2e87bade55b',
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true",
+        page: '1',
+        per_page: "27",
+
+};
+    
+ /*===================MARKUPDATA=========================*/
+const markupData = {
+    markup: "",
+    htmlCode: "",
+};
+
+ /*=====================ELEM=======================*/
 const elem = {
-    form : document.querySelector('.#search-form'),
-    gallery : document.querySelector('.gallery'),
-    button : document.querySelector('.load-more')
+ btnLoadMore : document.querySelector('.load-more'),
+ form : document.querySelector('.search-form'),
+ gallery : document.querySelector('.gallery'),
+  simpleLightbox : new SimpleLightbox('.gallery a'),
 }
 
- elem.button.addEventListener('click', buttonClick);
- function buttonClick () {
- }
+//default button invesible
+elem.btnLoadMore.style.display = 'none';
 
 
+ /*===================SUBMIT=========================*/
+ elem.form.addEventListener("submit", async (evt) => {
+    evt.preventDefault();
+
+    const { elements: { searchQuery } } = evt.target;
+    
+    //let
+    searchQueryResult = searchQuery.value;
+    if (searchQueryResult === '') {
+        elem.gallery.innerHTML = q;
+
+        elem.btnLoadMore.disabled = false;        
+        return Notify.failure('❌Sorry, there are no images matching your search query. Please try again.');
+            
+    };
+            //let
+    if (searchQueryResult !== q) {
+        //let
+        pageOne = 1;
+        pixabayAPI.page = `${pageOne}`;
+
+        elem.gallery.innerHTML = q;
+
+        elem.btnLoadMore.style.display = 'block';
+        elem.btnLoadMore.disabled = false;
+
+    } else {
+        pageOne += 1;
+        pixabayAPI.page = `${pageOne}`;
+        
+        elem.btnLoadMore.style.display = 'block';
+        elem.btnLoadMore.disabled = false;
+    };
+    
+    q = searchQueryResult;
+    
+    try {
+
+        const results = await fetchPhotos(searchQueryResult);
+        markupData.htmlCode = await renderPhotos(results);
+
+        elem.gallery.insertAdjacentHTML("beforeend", markupData.htmlCode);
+      
+        elem.btnLoadMore.style.display = 'block';     
+        elem.btnLoadMore.disabled = false;
+   
+        // simpleLightbox gallery 
+       elem.simpleLightbox.refresh();
+        
+        
+
+        const { baseUrl, key, image_type, orientation, safesearch, page, per_page } = pixabayAPI;
+        const { total, totalHits, images} = results;    
+        const totalPages = Math.ceil(totalHits / per_page);
+        
+        
+        if (page >= totalPages) { 
+
+            elem.btnLoadMore.style.display = 'block';
+        };
+        Notify.success(`'👌Hooray! We found ${results.totalHits} picture.'`);
+    }
+
+    catch (error) {
+        Notify.failure('❌Sorry, there are no images matching your search query. Please try again.');
+
+    };
+});
+
+ /*==================LOAD MORE==========================*/
+elem.btnLoadMore.addEventListener("click", async () => {
+
+        pageOne += 1;
+        pixabayAPI.page = `${pageOne}`;
+
+try {
+
+        const results = await fetchPhotos(searchQueryResult);
+        markupData.htmlCode = await renderPhotos(results);
+        
+        elem.gallery.insertAdjacentHTML("beforeend", markupData.htmlCode);
+        elem.btnLoadMore.style.display = 'block';      
+        elem.btnLoadMore.disabled = false;
   
+        // simpleLightbox gallery 
+       elem.simpleLightbox.refresh();
+
+        const { baseUrl, key, image_type, orientation, safesearch, page, per_page } = pixabayAPI;
+        const { total, totalHits,  hits } = results;    
+        const totalPages = Math.ceil(totalHits / per_page);
+        
+    if (page >= totalPages) {
+            
+        elem.btnLoadMore.style.display = 'block';
+        elem.btnLoadMore.disabled = false;
+
+        };
+
+        console.log("results", results);
+
+    }
+    catch (error) {   
+        Notify.failure(" ❌We're sorry, but you've reached the end of search results.");
+
+    }
+
+});
+
+ /*====================FETCH REQUEST========================*/
+async function fetchPhotos(searchQueryResult) {
+
+    const { baseUrl, key, image_type, orientation, safesearch, page, per_page } = pixabayAPI;
+
+
+    pixabayAPI.page = `${pageOne}`;
+    
+    const response = await axios.get(`${baseUrl}?key=${key}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&page=${page}&per_page=${per_page}`);
+    const results = response.data;
+   
+    const {total, totalHits,  hits } = results;
+    const totalPages = Math.ceil(totalHits/ per_page);
+    
+    if (total === 0) {
+    throw new Error();
+    };
+
+    console.log(totalHits);
+
+    //total pages check
+    if (page >= totalPages) {
+        
+        elem.btnLoadMore.style.display = 'block';
+        elem.btnLoadMore.disabled = false;
+
+        Notify.failure("❌We're sorry, but you've reached the end of search results.");
+        return results;
+
+    };
+    return results;
+};
+
+ /*====================RENDER========================*/
+async function renderPhotos(results) {
+
+    const { hits  } = results;
+
+    markupData.markup =  hits.map((hit ) =>
+        `<a href="${hit.largeImageURL}"><div class="photo-card">
+        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy"
+          class="img-item" />
+        <div class="info">
+    <p class="info-item">
+      <b>Likes:</b>${hit.likes}
+    </p>
+    <p class="info-item">
+      <b>Views:</b>${hit.views}
+    </p>
+    <p class="info-item">
+      <b>Comments:</b>${hit.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads:</b>${hit.downloads}
+    </p>
+  </div>
+</div></a>`).join("");
+    
+    return markupData.markup;
+    
+};
